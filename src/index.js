cb000b2184a3e252113d34db3d52a2190cce8b4b
Samwel Charles <younggeeks101@gmail.com>
2019-02-18 23:21:51 +0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require("express")
 const app = express()
 const models = require("./db/models")
+const SmsController = require("./controllers/SmsController")
+const bodyParser = require("body-parser")
 const {PORT} = process.env 
+const smsController = new SmsController()
 
-app.get("/", (req,res)=>{
-    res.send("Hello world , awesomeness will come your way soon")
-})
 
+app.use(bodyParser({json:true}))
+
+
+//messages endpoint
+app.get("/messages", smsController.getAll)
+app.get("/messages/:id", smsController.getOne)
+app.get("/messages/sent/:id",smsController.getAllBySender)
+app.get("/messages/received/:id",smsController.getAllByRecepient)
+
+app.post("/messages",smsController.sendNewMessage)
+
+//contacts endpoint
 
 
 models.sequelize.sync({force:false}).then(()=>{
