1434de94fa55c51e2ff8c5404d5b3829b5784866
Samwel Charles <younggeeks101@gmail.com>
2019-02-18 23:21:21 +0300

    add association/relationship to sms model

--- src/db/models/sms.js
+++ src/db/models/sms.js
@@ -1,11 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Sms = sequelize.define('Sms', {
-    message: DataTypes.STRING
+    message: DataTypes.STRING,
+    status: DataTypes.STRING
   }, {});
   Sms.associate = function(models) {
     // associations can be defined here
-    Sms.belongsTo(models.Contact)
+    Sms.belongsTo(models.Contact,{onDelete:'CASCADE',foreignKey:'senderId'})
+    Sms.belongsTo(models.Contact,{onDelete:'CASCADE', foreignKey:'receiverId'})
   };
   return Sms;
 };
