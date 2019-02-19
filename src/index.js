2937abdd6cf9fd42a580c459e475b6cd6aaa070a
Samwel Charles <younggeeks101@gmail.com>
2019-02-19 22:42:27 +0300

    add contacts routes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const app = express()
 const models = require("./db/models")
 const SmsController = require("./controllers/SmsController")
+const ContactsController = require("./controllers/ContactsController")
 const bodyParser = require("body-parser")
+const {validateMessage,validateContact} = require("./db/helpers/validation")
 const {PORT} = process.env 
 const smsController = new SmsController()
-
+const contactsController = new ContactsController()
 
 app.use(bodyParser({json:true}))
 
@@ -15,10 +17,12 @@ app.get("/messages", smsController.getAll)
 app.get("/messages/:id", smsController.getOne)
 app.get("/messages/sent/:id",smsController.getAllBySender)
 app.get("/messages/received/:id",smsController.getAllByRecepient)
-
-app.post("/messages",smsController.sendNewMessage)
+app.post("/messages",validateMessage,smsController.sendNewMessage)
 
 //contacts endpoint
+app.get("/contacts", contactsController.getContacts)
+app.get("/contacts/:id", contactsController.getSingleContact)
+app.post("/contacts",validateContact,contactsController.insertContact)
 
 
 models.sequelize.sync({force:false}).then(()=>{
